c37ea86a3ad8b327543024c57180cfa838090dae
unknown <SmithaRai.com>
2022-02-16 11:51:13 +0530

    added wage details

---
+++ conditionalwage.sh
@@ -0,0 +1,9 @@
+wageperhr=20
+wageperday=160
+totwrkinhrs=100
+wrkindays=20
+ echo "total working hours:$totwrkinhrs"
+ echo "wage as per working hours :$((wageperhr*totwrkinhrs))"
+ echo "total working days :$wrkindays"
+ echo "wage as per working days :$((wageperday*wrkindays))"
+
